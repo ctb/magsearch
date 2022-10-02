15e5bfa093864efc646b359051ff50747e3cd695
C. Titus Brown <titus@idyll.org>
2022-10-02 07:29:59 -0700

    [MRG] add results summary script using pandas (#4)
    This was broken out from a fairly heavily modified notebook based on
    https://github.com/dib-lab/2022-sra-gather/blob/main/summarize-sample.py,
    which was also used over
    [here](https://github.com/sourmash-bio/2022-search-sra-with-mastiff/blob/main/interpret-sra-live.ipynb).
    
    The `summarize-results.py` script takes in an SRA runinfo file and a
    MAGsearch CSV file, together with an optional containment threshold
    (defaults to 0.2) and produces the following outputs:
    * information about the number of rows being loaded and filtered out
    * an optional output file via `-o` that provides an additional
    `ScientificName` column on the input magsearch CSV.
    * a summary of the number of distinct metagenomes found, broken down by
    metagenome type.
    
    Example run:
    ```
    % ./summarize-results.py --run ~ctbrown/transfer/big.runinfo.csv.gz output.ibd_subset/results/ibd_subset.csv -o output.ibd_subset/results/ibd_subset.annot.csv
    
    loading runinfo from '/home/ctbrown/transfer/big.runinfo.csv.gz'
    ./summarize-results.py:89: DtypeWarning: Columns (8,33,38,39,40,43) have mixed types.Specify dtype option on import or set low_memory=False.
      sys.exit(main())
    ...done. Loaded 702013 rows.
    
    loading magsearch results from 'output.ibd_subset/results/ibd_subset.csv'
    ...done. Loaded 1297 rows.
    
    filtering magsearch results at a threshold of 0.20
    ...189 left.
    
    cleaning up magsearch result columns...
    joining magsearch results with run info...
    ignoring 26 magsearch rows b/c null ScientificName.
    
    Of 189 MAGsearch results, 163 have non-null metadata
    writing annotated MAGsearch results to 'output.ibd_subset/results/ibd_subset.annot.csv'
    ...wrote 163 results.
    
    163 distinct metagenomes found.
    
    Breakdown of ScientificName across distinct metagenomes:
    
    human gut metagenome      100
    human metagenome           20
    gut metagenome             13
    Homo sapiens               10
    metagenome                  8
    feces metagenome            8
    wastewater metagenome       2
    human feces metagenome      1
    sediment metagenome         1
    Name: ScientificName, dtype: int64
    ```
    
    with the output file
    ```
    % head output.ibd_subset/results/ibd_subset.annot.csv
    Run,query,containment,ScientificName
    SRR11304945,5b056fe3f37e848705a1764547bed6eb,0.2753527942077693,human gut metagenome
    SRR10680700,5b056fe3f37e848705a1764547bed6eb,0.2318278746939307,human gut metagenome
    ERR3521962,5b056fe3f37e848705a1764547bed6eb,0.3054591940014586,human metagenome
    SRR5279296,5b056fe3f37e848705a1764547bed6eb,0.2548520468857886,human gut metagenome
    ERR4097298,5b056fe3f37e848705a1764547bed6eb,0.2703808153158024,human gut metagenome
    ERR6618678,5b056fe3f37e848705a1764547bed6eb,0.211840131329013,human gut metagenome
    DRR171892,5b056fe3f37e848705a1764547bed6eb,0.208484808150385,human gut metagenome
    ERR2235305,5b056fe3f37e848705a1764547bed6eb,0.2110110060146247,human gut metagenome
    SRR8982776,5b056fe3f37e848705a1764547bed6eb,0.281156671408487,human gut metagenome
    ```
    
    TODO:
    
    - [x] integrate into snakefile
    - [x] add some tests
    - [x] consider using quotechar

---
+++ bin/summarize-results.py
@@ -0,0 +1,86 @@
+#! /usr/bin/env python
+"""
+Summarize magsearch results.
+"""
+import sys
+import pandas as pd
+import argparse
+import os
+
+
+def extract_run_acc(x):
+    "get just the basename of the run filename, with .sig extension removed"
+    # trim off path
+    x = os.path.basename(x)
+
+    # remove extension '.sig'
+    y, ext = os.path.splitext(x)
+
+    # confirm expected .sig ending ;)
+    assert ext == '.sig', ext
+
+    # done!
+    return y
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('--run-info-sra', required=True)
+    p.add_argument('magsearch_csv')
+    p.add_argument('-t', '--containment-threshold', type=float, default=0.20)
+    p.add_argument('-N', '--display-num-rows', type=int, default=20)
+    p.add_argument('-o', '--output-annotated-csv')
+    args = p.parse_args()
+
+    assert args.magsearch_csv != args.output_annotated_csv
+
+    print(f"loading runinfo from '{args.run_info_sra}'", file=sys.stderr)
+    run_info = pd.read_csv(args.run_info_sra)
+    print(f"...done. Loaded {len(run_info)} rows.", file=sys.stderr)
+    print("", file=sys.stderr)
+
+    print(f"loading magsearch results from '{args.magsearch_csv}'",
+          file=sys.stderr)
+    magsearch_df = pd.read_csv(args.magsearch_csv, quotechar="'")
+    print(f"...done. Loaded {len(magsearch_df)} rows.", file=sys.stderr)
+    print("", file=sys.stderr)
+
+    print(f"filtering magsearch results at a threshold of {args.containment_threshold:.02f}", file=sys.stderr)
+    magsearch_df = magsearch_df[magsearch_df.containment >= args.containment_threshold]
+    print(f"...{len(magsearch_df)} left.", file=sys.stderr)
+    print("", file=sys.stderr)
+    
+    # clean up the MAGsearch columns
+    print(f"cleaning up magsearch result columns...", file=sys.stderr)
+    magsearch_df['Run'] = magsearch_df['Run'].apply(extract_run_acc)
+
+    # join in the ScientificName column from run_info.
+    print(f"joining magsearch results with run info...", file=sys.stderr)
+    magsearch2_df = magsearch_df.set_index('Run').join(run_info.set_index('Run')['ScientificName'])
+
+    null_df = magsearch2_df[magsearch2_df['ScientificName'].isnull()]
+    print(f"ignoring {len(null_df)} magsearch rows b/c null ScientificName.",
+          file=sys.stderr)
+    print("", file=sys.stderr)
+
+    magsearch3_df = magsearch2_df[~magsearch2_df['ScientificName'].isnull()]
+    print(f"Of {len(magsearch2_df)} MAGsearch results, {len(magsearch3_df)} have non-null metadata", file=sys.stderr)
+
+    if args.output_annotated_csv:
+        print(f"writing annotated MAGsearch results to '{args.output_annotated_csv}'", file=sys.stderr)
+        magsearch3_df.to_csv(args.output_annotated_csv)
+        print(f"...wrote {len(magsearch3_df)} results.", file=sys.stderr)
+        print("", file=sys.stderr)
+
+    # distinct metagenome IDs
+    sra_acc_set = set(magsearch3_df.index)
+    
+    print(f"{len(sra_acc_set)} distinct metagenomes found.", file=sys.stderr)
+    distinct_metagenomes = run_info[run_info["Run"].isin(sra_acc_set)]
+
+    print("\nBreakdown of ScientificName across distinct metagenomes:\n", file=sys.stderr)
+    print(distinct_metagenomes["ScientificName"].value_counts()[:args.display_num_rows])
+
+
+if __name__ == '__main__':
+    sys.exit(main())
